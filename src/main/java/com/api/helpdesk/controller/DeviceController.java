7e45a9110cc68b2a444c206b454213f0153db4be
GabiTavaresV <gabriela.ttavares@gmail.com>
2024-12-16 14:15:55 -0300

    feat: update controller device

--- src/main/java/com/api/helpdesk/controller/DeviceController.java
+++ src/main/java/com/api/helpdesk/controller/DeviceController.java
@@ -4,6 +4,8 @@ package com.api.helpdesk.controller;
 import com.api.helpdesk.dto.DeviceDTO;
 import com.api.helpdesk.service.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,17 +18,20 @@ public class DeviceController {
     private DeviceService deviceService;
 
     @PostMapping
-    public DeviceDTO create(@RequestBody DeviceDTO device) {
-        return deviceService.createDevice(device);
+    public ResponseEntity<DeviceDTO> create(@RequestBody DeviceDTO device) {
+        DeviceDTO createDevice = deviceService.createDevice(device);
+        return new ResponseEntity<>(createDevice, HttpStatus.CREATED);
     }
 
     @GetMapping("/findAll")
-    public List<DeviceDTO> getAll() {
-        return deviceService.getAllDevices();
+    public ResponseEntity<List<DeviceDTO>> getAll() {
+        List<DeviceDTO> list = deviceService.getAllDevices();
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public DeviceDTO getById(@PathVariable Long id) {
-        return deviceService.getDeviceById(id);
+    public ResponseEntity<DeviceDTO> getById(@PathVariable Long id) {
+        DeviceDTO device = deviceService.getDeviceById(id);
+        return new ResponseEntity<>(device, HttpStatus.OK);
     }
 }
