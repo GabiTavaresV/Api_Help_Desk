b224de742a0a54c9e604acfa03c8a6a28a0d592e
GabiTavaresV <gabriela.ttavares@gmail.com>
2024-12-16 10:29:40 -0300

    feat: add lombok

--- src/main/java/com/api/helpdesk/dto/AttendantDTO.java
+++ src/main/java/com/api/helpdesk/dto/AttendantDTO.java
@@ -1,32 +1,17 @@
 package com.api.helpdesk.dto;
 
+
+import lombok.*;
+
+@Builder
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class AttendantDTO {
     private Long id;
     private String name;
 
-
-    public AttendantDTO() {
-        this.id = id;
-    }
-
-    public AttendantDTO(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/com/api/helpdesk/entity/Attendant.java
+++ src/main/java/com/api/helpdesk/entity/Attendant.java
@@ -2,10 +2,17 @@ package com.api.helpdesk.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Builder
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "attendant")
 public class Attendant {
@@ -18,22 +25,5 @@ public class Attendant {
     @OneToMany(mappedBy = "attendant")
     @JsonIgnore
     private List<Desk> desks = new ArrayList<>();
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 
 }
