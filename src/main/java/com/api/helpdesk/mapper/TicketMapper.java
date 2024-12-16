a3ba716010b03fef387230776a8d84194511b4aa
GabiTavaresV <gabriela.ttavares@gmail.com>
2024-12-16 11:56:05 -0300

    feat: add attendant Mapper

---
+++ src/main/java/com/api/helpdesk/mapper/TicketMapper.java
@@ -0,0 +1,65 @@
+package com.api.helpdesk.mapper;
+
+import com.api.helpdesk.dto.TicketDTO;
+import com.api.helpdesk.entity.Ticket;
+
+public class TicketMapper {
+
+    private final UserMapper userMapper = new UserMapper();
+    private final DeskMapper deskMapper = new DeskMapper();
+    private final DeviceMapper deviceMapper = new DeviceMapper();
+    private final AttendantMapper attendantMapper = new AttendantMapper();
+
+
+    public TicketDTO toDTO(Ticket ticket) {
+        if (ticket == null) {
+            return null;
+        }
+        TicketDTO ticketDTO = new TicketDTO();
+        ticketDTO.setId(ticket.getId());
+        ticketDTO.setReason(ticket.getReason());
+        ticketDTO.setStatus(ticket.getStatus());
+
+        if (ticket.getCustomer() != null) {
+            ticketDTO.setCustomer(userMapper.toDTO(ticket.getCustomer()));
+        }
+        if (ticket.getDevice() != null) {
+            ticketDTO.setDevice(deviceMapper.toDTO(ticket.getDevice()));
+        }
+
+        if (ticket.getDesk() != null) {
+            ticketDTO.setDesk(deskMapper.toDTO(ticket.getDesk()));
+        }
+
+        if (ticket.getAttendant() != null) {
+            ticketDTO.setAttendant(attendantMapper.toDTO(ticket.getAttendant()));
+        }
+
+
+        return ticketDTO;
+    }
+
+    public Ticket toEntity(TicketDTO ticketDTO) {
+        if (ticketDTO == null) {
+            return null;
+        }
+        Ticket ticket = new Ticket();
+        ticket.setId(ticketDTO.getId());
+        ticket.setReason(ticketDTO.getReason());
+        ticket.setStatus(ticketDTO.getStatus());
+
+        if (ticketDTO.getCustomer() != null) {
+            ticket.setCustomer(userMapper.toEntity(ticketDTO.getCustomer()));
+        }
+
+        if (ticketDTO.getDevice() != null) {
+            ticket.setDevice(deviceMapper.toEntity(ticketDTO.getDevice()));
+        }
+
+        if (ticketDTO.getDesk() != null) {
+            ticket.setDesk(deskMapper.toEntity(ticketDTO.getDesk()));
+        }
+
+        return ticket;
+    }
+}
--- src/main/java/com/api/helpdesk/service/TicketService.java
+++ src/main/java/com/api/helpdesk/service/TicketService.java
@@ -1,14 +1,10 @@
 package com.api.helpdesk.service;
 
-
 import com.api.helpdesk.dto.*;
-import com.api.helpdesk.entity.Desk;
-import com.api.helpdesk.entity.Device;
 import com.api.helpdesk.entity.Ticket;
-import com.api.helpdesk.entity.Users;
+import com.api.helpdesk.mapper.*;
 import com.api.helpdesk.repository.TicketRepository;
 import com.api.helpdesk.utils.TicketStatus;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -33,71 +29,86 @@ public class TicketService {
     @Autowired
     private DeviceService deviceService;
 
-    public Ticket createTicket(Long customerId, Long deskId, Long deviceId, String reason) {
-        Users user = userService.getUserById(customerId);
-        Desk desk = deskService.getDeskById(deskId);
-        Device device = deviceService.getDeviceById(deviceId);
+    @Autowired
+    private AttendantService attendantService;
+
+    private final TicketMapper ticketMapper = new TicketMapper();
+    private final DeskMapper deskMapper = new DeskMapper();
+    private final UserMapper userMapper = new UserMapper();
+    private final DeviceMapper deviceMapper = new DeviceMapper();
+    private final AttendantMapper attendantMapper = new AttendantMapper();
+
+    public TicketDTO createTicket(Long customerId, Long deskId, Long deviceId, String reason, Long attendantId) throws NotFoundDBException {
+
+        UserDTO user = userService.getUserById(customerId);
+        DeskDTO desk = deskService.getDeskById(deskId);
+        DeviceDTO device = deviceService.getDeviceById(deviceId);
+        AttendantDTO attendant = attendantService.getAttendantById(attendantId);
 
         Ticket ticket = new Ticket();
-        ticket.setCustomer(user);
-        ticket.setDesk(desk);
-        ticket.setDevice(device);
+        ticket.setCustomer(userMapper.toEntity(user));
+        ticket.setDesk(deskMapper.toEntity(desk));
+        ticket.setDevice(deviceMapper.toEntity(device));
         ticket.setReason(reason);
+        ticket.setAttendant(attendantMapper.toEntity(attendant));
+
         ticket.setCreatedDate(LocalDateTime.now());
         ticket.setStatus(TicketStatus.ABERTO);
 
-        return ticketRepository.save(ticket);
+        Ticket savedTicket = ticketRepository.save(ticket);
+        return ticketMapper.toDTO(savedTicket);
     }
 
     public List<Ticket> listAllTickets(Pageable pageable) {
         return ticketRepository.findAll(pageable).getContent();
     }
 
-    public Page<Ticket> listTicketsByCustomerId(Long customerId, Pageable pageable)  {
+    public Page<Ticket> listTicketsByCustomerId(Long customerId, Pageable pageable) {
         return ticketRepository.findByCustomerId(customerId, pageable);
     }
 
-    public TicketDTO getTicketDetails(Long id) throws NotFoundDBException {
+    public Ticket getTicketDetails(Long id) throws NotFoundDBException {
         Ticket ticket = ticketRepository.findById(id)
                 .orElseThrow(() -> new NotFoundDBException("Chamado não encontrado!"));
 
-        TicketDTO ticketDTO = new TicketDTO();
-        ticketDTO.setId(ticket.getId());
-        ticketDTO.setReason(ticket.getReason());
-        ticketDTO.setStatus(ticket.getStatus());
+//        TicketDTO ticketDTO = new TicketDTO();
+//        ticketDTO.setId(ticket.getId());
+//        ticketDTO.setReason(ticket.getReason());
+//        ticketDTO.setStatus(ticket.getStatus());
+//
+//        if (ticket.getCustomer() != null) {
+//            UserDTO userDTO = new UserDTO();
+//            userDTO.setId(ticket.getCustomer().getId());
+//            userDTO.setName(ticket.getCustomer().getName());
+//            userDTO.setEmail(ticket.getCustomer().getEmail());
+//
+//            ticketDTO.setCustomer(userDTO);
+//        }
+//
+//        if (ticket.getDevice() != null) {
+//            DeviceDTO deviceDTO = new DeviceDTO();
+//            deviceDTO.setId(ticket.getDevice().getId());
+//            deviceDTO.setSerialNumber(ticket.getDevice().getSerialNumber());
+//            ticketDTO.setDevice(deviceDTO);
+//        }
+//
+//        if (ticket.getDesk() != null) {
+//            DeskDTO deskDTO = new DeskDTO();
+//            deskDTO.setId(ticket.getDesk().getId());
+//
+//            ticketDTO.setDesk(deskDTO);
+//        }
+//
+//        if (ticket.getDesk().getAttendant() != null) {
+//            AttendantDTO attendantDTO = new AttendantDTO();
+//            attendantDTO.setId(ticket.getDesk().getAttendant().getId());
+//            attendantDTO.setName(ticket.getDesk().getAttendant().getName());
+//            ticketDTO.setAttendant(attendantDTO);
+//        }
+//
+//        return ticketDTO;
 
-        if (ticket.getCustomer() != null) {
-            UserDTO userDTO = new UserDTO();
-            userDTO.setId(ticket.getCustomer().getId());
-            userDTO.setName(ticket.getCustomer().getName());
-            userDTO.setEmail(ticket.getCustomer().getEmail());
-
-            ticketDTO.setCustomer(userDTO);
-        }
-
-        if (ticket.getDevice() != null) {
-            DeviceDTO deviceDTO = new DeviceDTO();
-            deviceDTO.setId(ticket.getDevice().getId());
-            deviceDTO.setSerialNumber(ticket.getDevice().getSerialNumber());
-
-            ticketDTO.setDevice(deviceDTO);
-        }
-
-        if (ticket.getDesk() != null) {
-            DeskDTO deskDTO = new DeskDTO();
-            deskDTO.setId(ticket.getDesk().getId());
-
-            ticketDTO.setDesk(deskDTO);
-        }
-
-        if (ticket.getDesk().getAttendant() != null) {
-            AttendantDTO attendantDTO = new AttendantDTO();
-            attendantDTO.setId(ticket.getDesk().getAttendant().getId());
-            attendantDTO.setName(ticket.getDesk().getAttendant().getName()); // ou outro campo relevante
-            ticketDTO.setAttendant(attendantDTO);
-        }
-
-        return ticketDTO;
+        return ticket;
     }
 
     public Page<Ticket> listTicketsByDeskId(Long deskId, Pageable pageable) {
