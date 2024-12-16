61217c207e22cd45381f2439866662325597c484
GabiTavaresV <gabriela.ttavares@gmail.com>
2024-12-16 11:56:20 -0300

    feat: add lombok

--- src/main/java/com/api/helpdesk/dto/TicketDTO.java
+++ src/main/java/com/api/helpdesk/dto/TicketDTO.java
@@ -1,6 +1,15 @@
 package com.api.helpdesk.dto;
 
 import com.api.helpdesk.utils.TicketStatus;
+import lombok.*;
+
+
+@Builder
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class TicketDTO {
 
@@ -9,62 +18,7 @@ public class TicketDTO {
     private TicketStatus status;
     private UserDTO customer;
     private DeviceDTO device;
-    private AttendantDTO attendant;
     private DeskDTO desk;
+    private AttendantDTO attendant;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-    public UserDTO getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(UserDTO customer) {
-        this.customer = customer;
-    }
-
-    public TicketStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(TicketStatus status) {
-        this.status = status;
-    }
-
-    public DeviceDTO getDevice() {
-        return device;
-    }
-
-    public void setDevice(DeviceDTO device) {
-        this.device = device;
-    }
-
-    public AttendantDTO getAttendant() {
-        return attendant;
-    }
-
-    public void setAttendant(AttendantDTO attendant) {
-        this.attendant = attendant;
-    }
-
-    public DeskDTO getDesk() {
-        return desk;
-    }
-
-    public void setDesk(DeskDTO desk) {
-        this.desk = desk;
-    }
 }
--- src/main/java/com/api/helpdesk/dto/TicketRequest.java
+++ src/main/java/com/api/helpdesk/dto/TicketRequest.java
@@ -1,41 +1,19 @@
 package com.api.helpdesk.dto;
 
+import lombok.*;
+
+@Builder
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class TicketRequest {
     private Long customerId;
     private Long deskId;
     private Long deviceId;
     private String reason;
+    private Long attendantId;
 
-
-    public Long getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Long customerId) {
-        this.customerId = customerId;
-    }
-
-    public Long getDeskId() {
-        return deskId;
-    }
-
-    public void setDeskId(Long deskId) {
-        this.deskId = deskId;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-    public Long getDeviceId() {
-        return deviceId;
-    }
-
-    public void setDeviceId(Long deviceId) {
-        this.deviceId = deviceId;
-    }
 }
