26a1ab4489f5c150a8ae2670f071e8a16117800b
GabiTavaresV <gabriela.ttavares@gmail.com>
2024-12-16 11:56:43 -0300

    feat: add lombok

--- src/main/java/com/api/helpdesk/entity/Ticket.java
+++ src/main/java/com/api/helpdesk/entity/Ticket.java
@@ -1,7 +1,5 @@
 package com.api.helpdesk.entity;
 
-
-
 import com.api.helpdesk.utils.TicketStatus;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
@@ -9,11 +7,15 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "ticket")
-//@Data
+@Builder
+@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "ticket")
+
 public class Ticket {
 
     @Id
@@ -23,16 +25,19 @@ public class Ticket {
     @ManyToOne
     @JoinColumn(name = "user_id")
     @JsonBackReference
-    private Users customer; // Mudado para um objeto Users
+    private Users customer;
 
     @ManyToOne
     @JoinColumn(name = "desk_id")
-    private Desk desk; // Mudado para um objeto Desk
+    private Desk desk;
 
-    // Se você tiver uma entidade Device, utilize isso para mapear
     @ManyToOne
     @JoinColumn(name = "device_id")
-    private Device device; // Mudado para um objeto Device
+    private Device device;
+
+    @ManyToOne
+    @JoinColumn(name = "attendant_id")
+    private Attendant attendant;
 
     private LocalDateTime createdDate;
     private LocalDateTime resolvedDate;
@@ -43,69 +48,4 @@ public class Ticket {
     private TicketStatus status;
 
 
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Users getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Users customer) {
-        this.customer = customer;
-    }
-
-    public Desk getDesk() {
-        return desk;
-    }
-
-    public void setDesk(Desk desk) {
-        this.desk = desk;
-    }
-
-    public Device getDevice() {
-        return device;
-    }
-
-    public void setDevice(Device device) {
-        this.device = device;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public LocalDateTime getResolvedDate() {
-        return resolvedDate;
-    }
-
-    public void setResolvedDate(LocalDateTime resolvedDate) {
-        this.resolvedDate = resolvedDate;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-    public TicketStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(TicketStatus status) {
-        this.status = status;
-    }
 }
