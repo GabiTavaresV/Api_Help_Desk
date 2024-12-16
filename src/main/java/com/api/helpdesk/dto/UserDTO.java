53d892d54609fb7e3714ba6577a882dac89e981b
GabiTavaresV <gabriela.ttavares@gmail.com>
2024-12-16 10:24:36 -0300

    feat: add lombok

--- src/main/java/com/api/helpdesk/dto/UserDTO.java
+++ src/main/java/com/api/helpdesk/dto/UserDTO.java
@@ -1,5 +1,15 @@
 package com.api.helpdesk.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class UserDTO {
 
     private Long id;
@@ -7,37 +17,4 @@ public class UserDTO {
     private String email;
 
 
-    public UserDTO() {
-
-    }
-
-    public UserDTO(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- src/main/java/com/api/helpdesk/entity/Users.java
+++ src/main/java/com/api/helpdesk/entity/Users.java
@@ -1,17 +1,20 @@
 package com.api.helpdesk.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Builder
-//@Data
+@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -28,36 +31,5 @@ public class Users {
     @JsonIgnore
     private List<Ticket> tickets;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<Ticket> getTickets() {
-        return tickets;
-    }
-
-    public void setTickets(List<Ticket> tickets) {
-        this.tickets = tickets;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
 }
