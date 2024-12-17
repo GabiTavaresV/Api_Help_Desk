53ae159528799599bc5f5a486f4bb15cdcfecb4c
GabiTavaresV <gabriela.ttavares@gmail.com>
2024-12-17 10:28:47 -0300

    config: remove commented line

--- src/main/java/com/api/helpdesk/repository/TicketRepository.java
+++ src/main/java/com/api/helpdesk/repository/TicketRepository.java
@@ -5,13 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     Page<Ticket> findByCustomerId(Long customerId, Pageable pageable);
-    List<Ticket> findByDeskId(Long deskId);
     Page<Ticket> findByDeskId(Long deskId, Pageable pageable);
-
-//    boolean existsByDeviceAndStatusIn(Device device, List<TicketStatus> statuses);
-
 }
