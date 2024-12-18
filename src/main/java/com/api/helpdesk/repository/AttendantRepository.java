b2a58ef597794bcc07d9c4f9b81206913352b4bd
GabiTavaresV <gabriela.ttavares@gmail.com>
2024-12-18 12:43:32 -0300

    feat: add filter for active attendant

--- src/main/java/com/api/helpdesk/repository/AttendantRepository.java
+++ src/main/java/com/api/helpdesk/repository/AttendantRepository.java
@@ -1,6 +1,7 @@
 package com.api.helpdesk.repository;
 
 import com.api.helpdesk.entity.Attendant;
+import com.api.helpdesk.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AttendantRepository extends JpaRepository<Attendant, Long> {
@@ -19,4 +21,10 @@ public interface AttendantRepository extends JpaRepository<Attendant, Long> {
 
     @Query("SELECT at FROM Attendant at WHERE at.isDeleted = false")
     List<Attendant> findAllActiveAttendants();
+
+    @Query("SELECT COUNT(at) > 0 FROM Attendant at WHERE at.name = :name AND at.isDeleted = false")
+    boolean existsByName(@Param("name") String email);
+
+    @Query("SELECT at FROM Attendant at WHERE at.id = :id AND at.isDeleted = false")
+    Optional<Attendant> findActiveAttendantById(@Param("id") Long id);
 }
--- src/main/java/com/api/helpdesk/service/AttendantService.java
+++ src/main/java/com/api/helpdesk/service/AttendantService.java
@@ -1,10 +1,14 @@
 package com.api.helpdesk.service;
 
+import com.api.helpdesk.controller.handler.EmailAlreadyExistsException;
 import com.api.helpdesk.dto.AttendantDTO;
 import com.api.helpdesk.entity.Attendant;
+import com.api.helpdesk.entity.Users;
 import com.api.helpdesk.exception.NotFoundDBException;
 import com.api.helpdesk.mapper.AttendantMapper;
+import com.api.helpdesk.mapper.UserMapper;
 import com.api.helpdesk.repository.AttendantRepository;
+import com.api.helpdesk.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,22 +24,29 @@ public class AttendantService {
 
     private final AttendantMapper attendantMapper = new AttendantMapper();
 
-    public AttendantDTO register(AttendantDTO attendantDTO) {
+    @Autowired
+    public AttendantService(AttendantRepository attendantRepository) {
+        this.attendantRepository = attendantRepository;
+    }
 
+    public AttendantDTO register(AttendantDTO attendantDTO) {
+        if (attendantRepository.existsByName(attendantDTO.getName())) {
+            throw new EmailAlreadyExistsException("Atendente já cadastrado.");
+        }
         Attendant attendant = attendantMapper.toEntity(attendantDTO);
         Attendant savedAttendant = attendantRepository.save(attendant);
         return attendantMapper.toDTO(savedAttendant);
     }
 
     public List<AttendantDTO> getAllAttendants() {
-        List<Attendant> devices = attendantRepository.findAll();
-        return devices.stream()
+        List<Attendant> attendants = attendantRepository.findAllActiveAttendants();
+        return attendants.stream()
                 .map(attendantMapper::toDTO)
                 .collect(Collectors.toList());
     }
 
     public AttendantDTO getAttendantById(Long id) throws NotFoundDBException {
-        Attendant attendant = attendantRepository.findById(id)
+        Attendant attendant = attendantRepository.findActiveAttendantById(id)
                 .orElseThrow(() -> new NotFoundDBException("Atendente não encontrado!"));
         return attendantMapper.toDTO(attendant);
     }
