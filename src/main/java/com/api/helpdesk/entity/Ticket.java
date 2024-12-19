7bd4e294d97a49f0f7df34fca78432cbd0395753
GabiTavaresV <gabriela.ttavares@gmail.com>
2024-12-19 19:30:30 -0300

    feat: add NotBlanck and NotNull

--- src/main/java/com/api/helpdesk/entity/Ticket.java
+++ src/main/java/com/api/helpdesk/entity/Ticket.java
@@ -3,6 +3,8 @@ package com.api.helpdesk.entity;
 import com.api.helpdesk.utils.TicketStatus;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -25,23 +27,35 @@ public class Ticket {
     @ManyToOne
     @JoinColumn(name = "user_id")
     @JsonBackReference
+    @NotNull(message = "O id do usuário não pode ser nulo.")
+    @NotBlank(message = "O id do usuário não pode ser vazio.")
     private Users customer;
 
     @ManyToOne
     @JoinColumn(name = "desk_id")
+    @NotNull(message = "O id do balcão não pode ser nulo.")
+    @NotBlank(message = "O id do balcão não pode ser vazio.")
     private Desk desk;
 
     @ManyToOne
     @JoinColumn(name = "device_id")
+    @NotNull(message = "O id do equipamento não pode ser nulo.")
+    @NotBlank(message = "O id do equipamento não pode ser vazio.")
     private Device device;
 
     @ManyToOne
     @JoinColumn(name = "attendant_id")
+    @NotNull(message = "O id do atendente não pode ser nulo.")
+    @NotBlank(message = "O id do atendente não pode ser vazio.")
     private Attendant attendant;
 
     private LocalDateTime createdDate;
+
+    @Column(name = "resolved_date")
     private LocalDateTime resolvedDate;
 
+    @NotNull(message = "O motivo não pode ser nulo.")
+    @NotBlank(message = "O motivo não pode ser vazio.")
     private String reason;
 
     @Enumerated(EnumType.STRING)
