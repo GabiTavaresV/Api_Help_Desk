b325d783ce6a37cfd160f0cb50b8629a51402acc
GabiTavaresV <gabriela.ttavares@gmail.com>
2024-12-19 19:30:10 -0300

    feat: add TicketConflictException

--- src/main/java/com/api/helpdesk/controller/handler/TicketConflictException.java
+++ src/main/java/com/api/helpdesk/controller/handler/TicketConflictException.java
@@ -1,4 +1,4 @@
-package com.api.helpdesk.handler;
+package com.api.helpdesk.controller.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
