14a3d87d81318749682fd506983d101b93a9513b
GabiTavaresV <gabriela.ttavares@gmail.com>
2024-12-19 19:32:54 -0300

    feat: add NotBlank and NotNull

--- src/main/java/com/api/helpdesk/entity/Attendant.java
+++ src/main/java/com/api/helpdesk/entity/Attendant.java
@@ -2,6 +2,8 @@ package com.api.helpdesk.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,6 +26,8 @@ public class Attendant {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "O nome não pode ser nulo.")
+    @NotBlank(message = "O nome não pode ser vazio.")
     private String name;
 
     @OneToMany(mappedBy = "attendant")
--- src/main/java/com/api/helpdesk/entity/Device.java
+++ src/main/java/com/api/helpdesk/entity/Device.java
@@ -1,6 +1,8 @@
 package com.api.helpdesk.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Builder
@@ -17,6 +19,8 @@ public class Device {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "O serial number não pode ser nulo.")
+    @NotBlank(message = "O serial number não pode ser vazio.")
     private String serialNumber;
 
     @Column(nullable = false)
