aa0eb81ec545f714837fc451b3f423d29a0ecf01
GabiTavaresV <gabriela.ttavares@gmail.com>
2024-12-19 19:28:00 -0300

    feat: add NotBlack and NotNull validation

--- src/main/java/com/api/helpdesk/entity/Users.java
+++ src/main/java/com/api/helpdesk/entity/Users.java
@@ -2,6 +2,8 @@ package com.api.helpdesk.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,7 +26,12 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "O nome não pode ser nulo.")
+    @NotBlank(message = "O nome não pode ser vazio.")
     private String name;
+
+    @NotNull(message = "O e-mail não pode ser nulo.")
+    @NotBlank(message = "O e-mail não pode ser vazio.")
     private String email;
 
     @OneToMany(mappedBy = "customer")
