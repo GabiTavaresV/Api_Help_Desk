87e5d201d33a7b706a88a05880b227e81a7e5009
GabiTavaresV <gabriela.ttavares@gmail.com>
2025-01-02 15:10:36 -0300

    fix: update moved from exception folder

--- src/main/java/com/api/helpdesk/exception/EmailAlreadyExistsException.java
+++ src/main/java/com/api/helpdesk/exception/EmailAlreadyExistsException.java
@@ -1,4 +1,4 @@
-package com.api.helpdesk.controller.handler;
+package com.api.helpdesk.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
