bd1176ed0afdc407cc2d7fedbacb8ea75f248f29
GabiTavaresV <gabriela.ttavares@gmail.com>
2025-01-03 16:28:39 -0300

    feat: add ticket repository

--- src/main/java/com/api/helpdesk/service/AttendantService.java
+++ src/main/java/com/api/helpdesk/service/AttendantService.java
@@ -1,11 +1,14 @@
 package com.api.helpdesk.service;
 
-import com.api.helpdesk.exception.EmailAlreadyExistsException;
+import com.api.helpdesk.exception.AttendantAlreadyExistsException;
 import com.api.helpdesk.dto.AttendantDTO;
 import com.api.helpdesk.entity.Attendant;
 import com.api.helpdesk.exception.NotFoundDBException;
+import com.api.helpdesk.exception.SoftDeleteException;
 import com.api.helpdesk.mapper.AttendantMapper;
 import com.api.helpdesk.repository.AttendantRepository;
+import com.api.helpdesk.repository.TicketRepository;
+import com.api.helpdesk.utils.TicketStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,12 +23,15 @@ public class AttendantService  {
     private AttendantRepository attendantRepository;
 
     @Autowired
+    private TicketRepository ticketRepository;
+
+    @Autowired
     private  AttendantMapper attendantMapper;
 
 
     public AttendantDTO register(AttendantDTO attendantDTO) {
         if (attendantRepository.existsByName(attendantDTO.getName())) {
-            throw new EmailAlreadyExistsException("Atendente já cadastrado.");
+            throw new AttendantAlreadyExistsException("Atendente já cadastrado.");
         }
         Attendant attendant = attendantMapper.toEntity(attendantDTO);
         Attendant savedAttendant = attendantRepository.save(attendant);
@@ -49,6 +55,14 @@ public class AttendantService  {
         if (deviceOptional.isEmpty()) {
             throw new NotFoundDBException("Atendente não encontrado!");
         }
+
+        long openTicketsCount = ticketRepository.countTicketsByAttendantIdAndNotConcluded(id, TicketStatus.CONCLUIDO);
+
+        if (openTicketsCount > 0) {
+            throw new SoftDeleteException("Não é possível deletar o atendente. Existem chamados abertos vinculados.");
+        }
+
+
         attendantRepository.softDeleteAttendantById(id);
         return null;
     }
