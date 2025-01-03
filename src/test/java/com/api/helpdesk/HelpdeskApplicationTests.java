4a639d11428be17f158a06235b47893b02be8bc4
GabiTavaresV <gabriela.ttavares@gmail.com>
2025-01-03 16:22:32 -0300

    test: update helpdeskAppication

--- src/test/java/com/api/helpdesk/HelpdeskApplicationTests.java
+++ src/test/java/com/api/helpdesk/HelpdeskApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class HelpdeskApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testMain() {
+		HelpdeskApplication.main(new String[0]);
 	}
 
 }
