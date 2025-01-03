57fa4e6e5de7af63fe8276ae4d1cf5c7cc6f0755
GabiTavaresV <gabriela.ttavares@gmail.com>
2025-01-03 16:27:14 -0300

    feat: add ticket repository

--- src/main/java/com/api/helpdesk/service/DeviceService.java
+++ src/main/java/com/api/helpdesk/service/DeviceService.java
@@ -2,10 +2,13 @@ package com.api.helpdesk.service;
 
 import com.api.helpdesk.dto.DeviceDTO;
 import com.api.helpdesk.entity.Device;
-import com.api.helpdesk.exception.ConflictException;
+import com.api.helpdesk.exception.DeviceAlreadyExistsException;
 import com.api.helpdesk.exception.NotFoundDBException;
+import com.api.helpdesk.exception.SoftDeleteException;
 import com.api.helpdesk.mapper.DeviceMapper;
 import com.api.helpdesk.repository.DeviceRepository;
+import com.api.helpdesk.repository.TicketRepository;
+import com.api.helpdesk.utils.TicketStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,12 +23,15 @@ public class DeviceService {
     private DeviceRepository deviceRepository;
 
     @Autowired
+    private TicketRepository ticketRepository;
+
+    @Autowired
     private  DeviceMapper deviceMapper;
 
     public DeviceDTO createDevice(DeviceDTO deviceDTO) {
         String serialNumber = deviceDTO.getSerialNumber();
         if (deviceRepository.existsBySerialNumber(serialNumber)) {
-            throw new ConflictException("Conflito: Já existe um dispositivo cadastrado com o mesmo serial number: " + serialNumber);
+            throw new DeviceAlreadyExistsException("Dispositivo já cadastrado.");
         }
         Device device = deviceMapper.toEntity(deviceDTO);
         Device savedDevice = deviceRepository.save(device);
@@ -45,8 +51,14 @@ public class DeviceService {
 
     public Void deleteDeviceById(Long id) throws NotFoundDBException {
         Optional<Device> deviceOptional = deviceRepository.findById(id);
-        if (!deviceOptional.isPresent()) {
+        if (deviceOptional.isEmpty()) {
             throw new NotFoundDBException("Equipamento não encontrado!");
+        }
+
+        long activeTicketsCount = ticketRepository.countTicketsByDeviceIdAndNotConcluded(id, TicketStatus.CONCLUIDO);
+
+        if (activeTicketsCount > 0) {
+            throw new SoftDeleteException("Não é possível deletar o dispositivo. Existem chamados não concluídos vinculados.");
         }
         deviceRepository.softDeleteByDeviceId(id);
         return null;
