fd971cfb7f232faac17500720c2ee3f0712968c6
GabiTavaresV <gabriela.ttavares@gmail.com>
2025-01-07 16:58:36 -0300

    fix: update mapper

--- src/main/java/com/api/helpdesk/mapper/TicketMapper.java
+++ src/main/java/com/api/helpdesk/mapper/TicketMapper.java
@@ -1,56 +1,42 @@
 package com.api.helpdesk.mapper;
 
-import org.springframework.stereotype.Component;
-
 import com.api.helpdesk.dto.TicketDTO;
 import com.api.helpdesk.entity.Ticket;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 import java.time.LocalDateTime;
 
-@Component
-public class TicketMapper {
+@Mapper(componentModel = "spring", uses = { UserMapper.class, DeviceMapper.class, DeskMapper.class })
+public interface TicketMapper {
 
-     UserMapper userMapper;
-    DeskMapper deskMapper;
-    DeviceMapper deviceMapper;
+    UserMapper userMapper = Mappers.getMapper(UserMapper.class);
+    DeviceMapper deviceMapper = Mappers.getMapper(DeviceMapper.class);
+    DeskMapper deskMapper = Mappers.getMapper(DeskMapper.class);
 
-    public TicketDTO toDTO(Ticket ticket) {
-        if (ticket == null) {
-            return null;
-        }
-        TicketDTO ticketDTO = new TicketDTO();
-        ticketDTO.setId(ticket.getId());
-        ticketDTO.setReason(ticket.getReason());
-        ticketDTO.setStatus(ticket.getStatus());
-        ticketDTO.setIsDeleted(ticket.getIsDeleted());
-        ticketDTO.setCreatedAt(ticket.getCreatedAt());
-        ticketDTO.setUpdatedAt(ticket.getUpdatedAt());
+    @Mapping(source = "customer", target = "customer")
+    @Mapping(source = "device", target = "device")
+    @Mapping(source = "desk", target = "desk")
+    TicketDTO toDTO(Ticket ticket);
 
-        if (ticket.getCustomer() != null) {
-            ticketDTO.setCustomer(userMapper.toDTO(ticket.getCustomer()));
-        }
-        if (ticket.getDevice() != null) {
-            ticketDTO.setDevice(deviceMapper.toDto(ticket.getDevice()));
-        }
+    @Mapping(source = "customer", target = "customer")
+    @Mapping(source = "device", target = "device")
+    @Mapping(source = "desk", target = "desk")
 
-        if (ticket.getDesk() != null) {
-            ticketDTO.setDesk(deskMapper.deskToDeskDTO(ticket.getDesk()));
-        }
-
-        return ticketDTO;
-    }
-
-    public Ticket toEntity(TicketDTO ticketDTO) {
+    default Ticket toEntity(TicketDTO ticketDTO) {
         if (ticketDTO == null) {
             return null;
         }
+
         Ticket ticket = new Ticket();
         ticket.setId(ticketDTO.getId());
         ticket.setReason(ticketDTO.getReason());
         ticket.setStatus(ticketDTO.getStatus());
         ticket.setUpdatedAt(LocalDateTime.now());
         ticket.setCreatedAt(LocalDateTime.now());
-        ticket.setIsDeleted(ticketDTO.getIsDeleted()!= null ? ticketDTO.getIsDeleted() : false);
+
+        ticket.setIsDeleted(ticketDTO.getIsDeleted() != null ? ticketDTO.getIsDeleted() : false);
 
         if (ticketDTO.getCustomer() != null) {
             ticket.setCustomer(userMapper.toEntity(ticketDTO.getCustomer()));
@@ -67,3 +53,4 @@ public class TicketMapper {
         return ticket;
     }
 }
+
